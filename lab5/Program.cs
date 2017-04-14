db1241f01b26ddad6b00b7a22f3557b8ff664991
Lanna <Lanna_lmb@hotmail.com>
2017-04-14 09:23:49 -0400

    Added Method for Outcome of Dice Roll

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -26,35 +26,11 @@ namespace lab5
                 int randomNumber = GetRandom();
                 Console.WriteLine();
                 Console.WriteLine(randomNumber);
-                int randomNumber1 = GetRandom1();
+                int randomNumber1 = GetRandom();
                 Console.WriteLine();
                 Console.WriteLine(randomNumber1);
-
-
-                // Determine Dice Rolled
-                if (randomNumber == 1 && randomNumber1 == 1)
-                {
-                    Console.WriteLine("SNAKE EYES AND YOU'RE A LOSER!!!");
-
-                }
-                else if (randomNumber + randomNumber1 == 7 || randomNumber + randomNumber1 == 11)
-
-                {
-                    Console.WriteLine("WINNER!!");
-                }
-
-                else if (randomNumber + randomNumber1 == 2 || randomNumber + randomNumber1 == 3|| randomNumber+randomNumber1==12 )
-                {
-                    Console.WriteLine("YOU ARE A LOSER!!");
-
-                }
-
-                else if ( randomNumber == 6 && randomNumber1 == 6)
-                {
-                    Console.WriteLine("BOX CARS");
-
-                }
-
+                PrintMessage(randomNumber, randomNumber1);
+               
 
                 // Try Again?
                 Console.WriteLine("Do You Want to Try Again?" + " " + "(Y/N)"); // add user input if they want to try again
@@ -65,43 +41,64 @@ namespace lab5
                 {
                     Console.WriteLine("Thanks for Playing!");
                     return;
-                  }
                 }
+            }
 
 
-                // Continue Loop
-                while (askToContinue);
-                }
+            // Continue Loop
+            while (askToContinue);
+        }
 
 
-                // Method get Valid Int
-                public static int GetValidInteger()
-                {
-                int input;
-                Console.WriteLine("Please Enter the Number of Sides for a Pair of Dice: ");
-                Console.WriteLine();
-                while (!int.TryParse(Console.ReadLine(), out input))
-                {
+        // Method get Valid Int
+        public static int GetValidInteger()
+        {
+            int input;
+            Console.WriteLine("Please Enter the Number of Sides for a Pair of Dice: ");
+            Console.WriteLine();
+            while (!int.TryParse(Console.ReadLine(), out input))
+            {
                 Console.WriteLine("Invalid Entry!  Please Enter an Integer!");
 
-                }
-                return input;
-                }
+            }
+            return input;
+        }
 
-                public static int GetRandom()
-                {
+        public static int GetRandom()
+        {
 
-                Random random = new Random(Guid.NewGuid().GetHashCode());
-                int randomNumber = random.Next(1, 6);
-                return randomNumber;
-                }
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            int randomNumber = random.Next(1, 6);
+            return randomNumber;
+        }
 
-                public static int GetRandom1()
-                {
 
-                Random random1 = new Random();
-                int randomNumber1 = random1.Next(1, 6);
-                return randomNumber1;
+        //Method for Outcome
+        public static void PrintMessage(int randomNumber, int randomNumber1)
+        {
+            // Determine Dice Rolled
+            if (randomNumber == 1 && randomNumber1 == 1)
+            {
+                Console.WriteLine("SNAKE EYES AND YOU'RE A LOSER!!!");
+
+            }
+            else if (randomNumber + randomNumber1 == 7 || randomNumber + randomNumber1 == 11)
+
+            {
+                Console.WriteLine("WINNER!!");
+            }
+
+            else if (randomNumber + randomNumber1 == 2 || randomNumber + randomNumber1 == 3 || randomNumber + randomNumber1 == 12)
+            {
+                Console.WriteLine("YOU ARE A LOSER!!");
+
+            }
+
+            else if (randomNumber == 6 && randomNumber1 == 6)
+            {
+                Console.WriteLine("BOX CARS");
+
+            }
         }
     }
 }
